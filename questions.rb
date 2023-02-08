1fac41500ebed3116d9730ab99922ca38fb9296e
Sean Abbas <sean.abbas33@gmail.com>
2023-02-08 16:29:39 -0500

    questions.rb created

---
+++ questions.rb
@@ -0,0 +1,19 @@
+require 'sqlite3'
+require 'singleton'
+
+class QuestionDatabase < SQLite3::Database
+  include Singleton
+
+  def initialize
+    super('questions.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
+
+class Question
+  def 
+
+
+
+end
