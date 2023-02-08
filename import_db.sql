3a0cf2bb86d3f3a28bf4db4c0efcf4a258bd178b
Naoko Sugawara <infoseek122@gmail.com>
2023-02-08 11:19:10 -0500

    create tables

--- import_db.sql
+++ import_db.sql
@@ -1,3 +1,6 @@
+PRAGMA foreign_keys = ON;
+
+
 
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
@@ -22,3 +25,26 @@ CREATE TABLE question_follows (
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (question_id) REFERENCES questions(id)
 );
+
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
+    body VARCHAR NOT NULL,
+    question_id INTEGER NOT NULL,
+    parent_reply_id INTEGER,
+    user_id INTEGER NOT NULL,
+
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (parent_reply_id) REFERENCES replies(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE questions_likes (  
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+
